fbfc4be80b47c7f731d1893f36ca010891e694ea
mesafintminwagaw23@gmail.com <mesafintminwagaw23@gmail.com>
2023-07-07 09:43:10 +0300

    finished converting figma to website

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,9 +1,9 @@
 import  SubHeading  from '../../components/SubHeading/SubHeading';
-import images from '../../constants/images';
-import data from '../../constants/data';
+import images from '../../constants/images.jsx';
+import data from '../../constants/data.jsx';
 import './Laurels.css';
 
-const AwardCard = ({  imgUrl, title, subtitle  }) => (
+const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
   <div className="app__laurels_awards-card">
     <img src={imgUrl} alt="awards" />
     <div className="app__laurels_awards-card_content">
